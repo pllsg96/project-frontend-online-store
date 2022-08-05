cc8249530016232c0d7b2fd9b980ce74120d1f42
Guilherme Crupi <guilhermecrupi@hotmail.com>
2022-08-05 19:10:38 -0300

    grupo 24 - Requisito 2 finalizado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Route exact path="/shoppingcart" component={ ShoppingCart } />
         <Route exact path="/" component={ Home } />
       </BrowserRouter>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <Link
+          data-testid="shopping-cart-button"
+          to="/shoppingcart"
+        >
+          ShoppingCart
+        </Link>
         <input type="text" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
